292ded5aa638d7f29a9a9e812978f4ab184c97e7
Logan Lucas <logs10658@gmail.com>
2026-02-07 10:30:58 -0600

    added branching

--- src/data/store.ts
+++ src/data/store.ts
@@ -5,11 +5,13 @@ interface GameStore {
     network: number;
     gpa: number;
     money: number;
+    branch: number;
 
     updateTechSkills: (value: number) => void;
     updateNetwork: (value: number) => void;
     updateGpa: (value: number) => void;
     updateMoney: (value: number) => void;
+    updateBranch: (value: number) => void;
 }
 
 export const useGameStore = create<GameStore>((set) => ({
@@ -17,10 +19,12 @@ export const useGameStore = create<GameStore>((set) => ({
     network: 0,
     gpa: 0,
     money: 0,
+    branch: 0,
     updateTechSkills: (value) =>
         set((state) => ({ techSkills: state.techSkills + value })),
     updateNetwork: (value) =>
         set((state) => ({ network: state.network + value })),
     updateGpa: (value) => set((state) => ({ gpa: state.gpa + value })),
     updateMoney: (value) => set((state) => ({ money: state.money + value })),
+    updateBranch: (value) => set({ branch: value }),
 }));
